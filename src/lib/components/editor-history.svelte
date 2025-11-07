f755a3d4cea4744261e067b09d6f6d49b5c3cab6
Riley1101 <arkardev00@gmail.com>
2025-11-07 17:19:53 -0500

    Wip added undo tree

--- src/lib/components/editor-history.svelte
+++ src/lib/components/editor-history.svelte
@@ -8,7 +8,4 @@
   <p>
     Editor history
   </p>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugiat incidunt quis? Animi delectus eius et magni molestias natus possimus quod soluta temporibus ullam? Esse eum laudantium porro quibusdam recusandae.
-  </p>
 </div>
