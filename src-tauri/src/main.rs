3555d6f42221c39c31a3aba6375756662e8231a5
Arkar Myat <arkardev00@gmail.com>
2026-01-31 11:28:14 -0500

    Update env path fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use fix_path_env;
+
 #[tokio::main]
 async fn main() {
+    fix_path_env::fix();
     tauri::async_runtime::set(tokio::runtime::Handle::current());
     memoire_lib::run().await;
 }
